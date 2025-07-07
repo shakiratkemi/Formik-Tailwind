e91a58808dc7fe87674250fe7ef90223da8b207a
shakirat <shakiratganiyy@gmail.com>
2025-07-07 09:40:11 +0100

    Updated the page

---
+++ form-react-tailwind/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <span className="text-3xl text-red font-bold underline border border-[red]">
+        Hello world!
+      </span>
+    </div>
+  );
+}
+
+export default App;
